f494ee984f23bec0e9ccb74939970290d02e0983
Andrés Aguirre <andres@andres-laptop2.(none)>
2011-05-03 03:08:16 -0300

    cambios ta nuevo plugins

--- trunk/turtle_art_plugin/butia_support/bobot_init.sh
+++ trunk/turtle_art_plugin/butia_support/bobot_init.sh
@@ -1,28 +1,5 @@
 #!/bin/bash
 
-while [ -n "$2" ] ; do
-    case "$1" in
-        -b | --bundle-id)     export SUGAR_BUNDLE_ID="$2" ;;
-        -a | --activity-id)   export SUGAR_ACTIVITY_ID="$2" ;;
-        -o | --object-id)     export SUGAR_OBJECT_ID="$2" ;;
-        -u | --uri)           export SUGAR_URI="$2" ;;
-        *) echo unknown argument $1 $2 ;;
-    esac
-    shift;shift
-done
-
-
-if [ "$SUGAR_BUNDLE_PATH" != "" ] ; then
-
-export PYTHONPATH=$SUGAR_BUNDLE_PATH/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=$SUGAR_BUNDLE_PATH/bin/lib:$LD_LIBRARY_PATH
-export PATH=$SUGAR_BUNDLE_PATH/bin:$PATH
-cd $SUGAR_BUNDLE_PATH
-
-fi
-
-cd plugins/butia_support/bobot
+cd plugins/butia/butia_support/bobot
 
 ./lua bobot-server.lua &
-
-
