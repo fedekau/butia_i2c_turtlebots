cb4227750b26f763530f7d03cd255ce96d91c62b
Jorge <xxopxe@gmail.com>
2011-06-09 21:27:03 -0300

    Buialo connects directly to bobot, skipping bobot-server.

---
+++ trunk/Butialo.activity/butialo-s.lua
@@ -0,0 +1,91 @@
+require "socket"
+
+local host, port = "localhost", 2009
+local bobot = assert(socket.connect(host, port))
+bobot:settimeout(nil) --blocking
+
+local debug = function() end
+--local debug=function(...) print("BL", ...) end
+
+function query_bobot(s)
+	debug("sending", s)
+	bobot:send(s.."\n")
+	local ret = bobot:receive()
+	debug("ret:", ret)
+	return ret
+end
+
+local function build_devices()
+	local d = {}
+	local list = query_bobot("LIST")
+	for module in string.gmatch(list, "%w+") do
+		local device={}
+
+		local desc=query_bobot("DESCRIBE "..module)
+		debug(module, desc)
+
+		local api=loadstring("return "..desc)()
+		if not api then
+			debug("Failure processing:", desc)
+			break
+		end
+		for func, desc in pairs(api) do
+			--[[
+			--TODO generar la funcion en un string para loadstring c/#parametros correcto
+			local callstring="CALL "..module.." "..func
+			device[func] = function(...)
+ 				for _, parameter in ipairs({...}) do
+					callstring=callstring.." "..parameter
+				end
+				return query_bobot(callstring)
+			end
+			--]]
+
+			debug ("---", func, desc)
+
+			local nparams=#desc.parameters
+			local generator = "return function("
+			local comma=""
+			for i=1,nparams do
+				generator=generator..comma.."p"..i --parameters
+				comma=","
+			end
+			generator=generator..") callstring='CALL "..module.." " ..func.." '"
+			for i=1,nparams do
+				generator=generator.."..(".."p"..i.." or '')" --parameters
+			end
+			generator=generator.." return query_bobot(callstring) end"
+		
+			debug("=========================")
+			debug(generator)
+			debug("=========================")
+	
+			device[func] = loadstring(generator)()
+
+		end
+
+		local devicename = string.upper(string.sub(module, 1, 1))
+			.. string.lower(string.sub(module, 2)) --lleva a "Boton"
+
+		d[devicename]=device
+		device.name=devicename
+	end
+
+	--local meta = { __index}
+	--setmetatable(d, meta)
+	--setmetatable(n, meta)
+
+	return d
+end
+
+
+
+-------------------------------------------
+--export stuff
+wait = socket.sleep
+DEVICES = build_devices()
+for n, d in pairs(DEVICES) do
+	debug("adding global", n, d)
+	_G[n]=d
+end
+
--- trunk/Butialo.activity/parse_bobot_tree.lua
+++ trunk/Butialo.activity/parse_bobot_tree.lua
@@ -1,49 +1,34 @@
 #!/bin/lua
 require "socket"
 
-local host, port = "localhost", 2009
-local bobot = assert(socket.connect(host, port))
-bobot:settimeout(nil) --blocking
+local butialo=require "butialo"
 
 local debug = function() end
 --local debug=function(...) print("PBT", ...) end
 
-local function query_bobot(s)
-	debug("sending", s)
-	bobot:send(s.."\n")
-	local ret = bobot:receive()
-	debug("ret:", ret)
-	return ret
-end
-
-
 local path = arg[1]
 
 debug("path", path)
+local devices = read_devices_list()
 
 if path=="/" then
-	local list = query_bobot("LIST")
-	for module in string.gmatch(list, "%w+") do
+	for module,_ in pairs(devices) do
 		print(module)
 	end
 	return
-end
-
-local desc = query_bobot("DESCRIBE "..path)
-local api=loadstring("return "..desc)()
-if not api then
-	debug("Failure processing:", desc)
-	return
-end
-for func, desc in pairs(api) do
-	local nparams=#desc.parameters
-	local generator = func.."( "
-	local comma=""
-	for i=1,nparams do
-		generator=generator..comma..(desc.parameters[i].rname or "p"..i) --parameters
-		comma=","
+else
+	local device = devices[path]
+	if device and device.api then
+		for func, desc in pairs(device.api) do
+			local nparams=#desc.parameters
+			local generator = func.."( "
+			local comma=""
+			for i=1,nparams do
+				generator=generator..comma..(desc.parameters[i].rname or "p"..i) --parameters
+				comma=","
+			end
+			generator=generator.." )"
+			print (generator)
+		end
 	end
-	generator=generator.." )"
-	print (generator)
 end
-
