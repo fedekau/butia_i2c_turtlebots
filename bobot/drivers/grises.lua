adadaefe4a35f5cff6c139d045bd4b831a31077c
Alan Aguiar <alanjas@hotmail.com>
2012-06-25 19:12:27 -0300

    update grises to new getValue

--- bobot/drivers/grises.lua
+++ bobot/drivers/grises.lua
@@ -1,22 +1,19 @@
 local device = _G
 
-local char00=string.char(0x00)
+local GET_VALUE=string.char(0x00)
 local string_byte=string.byte
 
+-- description: lets us know grey sensor's current value
+-- input: empty
+-- output: grey sensor's current value.
 api={}
-api.getLevel = {}
-api.getLevel.parameters = {} -- -- no input parameters
-api.getLevel.returns = {[1]={rname="par1", rtype="int"}} 
-api.getLevel.call = function ()
-	device:send(char00) -- codigo de operacion 0
-	local sen_anl_response = device:read(3) 
-	local raw_val	
-	if not sen_anl_response or string_byte(sen_anl_response or "00000000", 2) == nil or string_byte(sen_anl_response or "00000000", 3) == nil
-	then 
-		raw_val = "nil value"
-	else
-		raw_val = string_byte(sen_anl_response, 2)* 256 + string_byte(sen_anl_response, 3) 
-	end	
-	return raw_val 
-	
+api.getValue = {}
+api.getValue.parameters = {} -- no input parameters
+api.getValue.returns = {[1]={rname="par1", rtype="int"}} 
+api.getValue.call = function ()
+	device:send(GET_VALUE) -- operation code 1 = get grey level
+	local sen_anl_response = device:read(3) -- operation code and data
+	if not sen_anl_response or #sen_anl_response~=3	then return -1 end
+    local raw_val = math.floor(((string_byte(sen_anl_response, 2) or 0) + (string_byte(sen_anl_response, 3) or 0)* 256)/ 100)
+	return raw_val
 end
