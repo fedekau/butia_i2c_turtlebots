6a1a088e7fd4a40439416c2ec9cc3886208200f5
Andres Aguirre Dorelo <aguirrea@gmail.com>
2012-08-07 21:20:05 -0300

    new functions added to move set the mode of the motors (servo/wheel) and set position of a motor

--- bobot/drivers/ax.lua
+++ bobot/drivers/ax.lua
@@ -16,3 +16,38 @@ api.write_info.call = function (id, regstart, value)
     return raw_val
 end
 
+api.wheel_mode = {}
+api.wheel_mode.parameters = {[1]={rname="id", rtype="number", min=0, max=255}}
+api.wheel_mode.returns = {}
+api.wheel_mode.call = function (id )
+        id = tonumber(id)
+		local ret = device:send(string.char(WRITE_INFO,id,0x06,0x00,0x00))
+        local write_info_response = device:read(1) or string.char(0,0)
+		local ret = device:send(string.char(WRITE_INFO,id,0x08,0x00,0x00))
+        local write_info_response = device:read(1) or string.char(0,0)
+    end
+
+api.joint_mode = {}
+api.joint_mode.parameters = {[1]={rname="id", rtype="number", min=0, max=255},[2]={rname="minimo", rtype="number", min=0, max=1023},[3]={rname="maximo", rtype="number", min=0, max=1023}}
+api.joint_mode.returns = {}
+api.joint_mode.call = function (id ,minimo, maximo)
+        id = tonumber(id)
+        minimo=tonumber(minimo)
+        maximo=tonumber(maximo)
+		local ret = device:send(string.char(WRITE_INFO,id,0x06,math.floor(minimo / 256),minimo % 256))
+        local write_info_response = device:read(1) or string.char(0,0)
+		local ret = device:send(string.char(WRITE_INFO,id,0x08,math.floor(maximo / 256),maximo % 256))
+        local write_info_response = device:read(1) or string.char(0,0)
+    end
+
+api.set_position = {}
+api.set_position.parameters = {[1]={rname="id", rtype="number", min=0, max=255},[2]={rname="pos", rtype="number", min=0, max=1023}}
+api.set_position.returns = {}
+api.set_position.call = function (id, pos )
+        id = tonumber(id)
+        pos = tonumber(pos)
+        local ret = device:send(string.char(WRITE_INFO,id,0x1E,math.floor(pos / 256),pos % 256))
+        local write_info_response = device:read(1) or string.char(0,0)
+    end
+
+
