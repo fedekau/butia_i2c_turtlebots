d487b1d495a096da04876590d1443fcd4515a40d
Alan Aguiar <alanjas@hotmail.com>
2013-01-18 06:56:50 -0200

    add setvelmtr to driver motors (no firmware implented yet)

--- pybot/drivers/motors.py
+++ pybot/drivers/motors.py
@@ -1,6 +1,7 @@
 
 RD_VERSION = 0x00
 SET_VEL_2MTR = 0x01
+SET_VEL_MTR = 0x01
 
 f1 = {
     'name': 'getVersion',
@@ -9,11 +10,19 @@ f1 = {
     'read': 3
 }
 
+# not implemented
 f2 = {
+    'name': 'setvelmtr',
+    'call': SET_VEL_2MTR,
+    'params': 3,
+    'read': 1
+}
+
+f3 = {
     'name': 'setvel2mtr',
     'call': SET_VEL_2MTR,
     'params': 6,
     'read': 1
 }
 
-FUNCTIONS = [f1, f2]
+FUNCTIONS = [f1, f3]
