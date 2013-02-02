0ed6aee0a340a2308b6350d883c890128fb85f8f
Alan Aguiar <alanjas@hotmail.com>
2013-02-02 00:14:54 -0200

    add thread in pybot server

--- pybot/pybot_server.py
+++ pybot/pybot_server.py
@@ -5,42 +5,25 @@
 #
 
 import socket
+from threading import Thread
 import usb4butia
 
 
 PYBOT_HOST = 'localhost'
 PYBOT_PORT = 2009
 
-class server():
 
-    def __init__(self):
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self.socket.bind((PYBOT_HOST, PYBOT_PORT))
-        self.socket.listen(4)
+class Client(Thread):
+    def __init__(self, socket, addr, robot):
+        Thread.__init__(self)
+        self.sc = socket
+        self.addr = addr
+        self.robot = robot
 
-        self.robot = usb4butia.USB4Butia()
+    def run(self):
 
-    def call_aux(self, modulename, board_number, number, function, params):
-        params = params.split(' ')
-        print 'call aux', params
-        par = []
-        if modulename == 'motors':
-            if function == 'setvel2mtr':
-                if len(params) == 4:
-                    return self.robot.set2MotorSpeed(int(params[0]), int(params[1]), int(params[2]), int(params[3]))
-                elif len(params) == 6:
-                    for e in params:
-                        par.append(int(e))
-                    return self.robot.callModule(modulename, board_number, number, function, par)
-        else:
-            return self.robot.callModule(modulename, board_number, number, function, par)
-
-    def init_server(self):
-
-        self.sc, self.addr = self.socket.accept()
-        run = True
-        while run:
+        alive = True
+        while alive:
             result = ''
             rec = self.sc.recv(1024)
             
@@ -55,7 +38,7 @@ class server():
             if len(r) > 0:
                 if r[0] == 'QUIT':
                     result = 'BYE'
-                    run = False
+                    alive = False
 
                 elif r[0] == 'LIST':
                     l = self.robot.get_modules_list()
@@ -96,17 +79,52 @@ class server():
 
             else:
                 print 'null message'
+                self.sc.send('\n')
+          
+        print 'Closing...', self.addr
+        self.sc.close()
 
 
-          
-        print 'Closing..'  
-          
-        self.sc.close()  
-        self.socket.close()
-        self.robot.close()
+    def call_aux(self, modulename, board_number, number, function, params):
+        params = params.split(' ')
+        print 'call aux', params
+        par = []
+        if modulename == 'motors':
+            if function == 'setvel2mtr':
+                if len(params) == 4:
+                    return self.robot.set2MotorSpeed(int(params[0]), int(params[1]), int(params[2]), int(params[3]))
+                elif len(params) == 6:
+                    for e in params:
+                        par.append(int(e))
+                    return self.robot.callModule(modulename, board_number, number, function, par)
+        else:
+            return self.robot.callModule(modulename, board_number, number, function, par)
+
+
+class Server():
+
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.socket.bind((PYBOT_HOST, PYBOT_PORT))
+        self.socket.listen(4)
+
+        self.robot = usb4butia.USB4Butia()
+
+    def init_server(self):
+
+        self.sc, self.addr = self.socket.accept()
+
+        print "conectado a "+ str(self.addr)
+
+        t = Client(self.sc, self.addr, self.robot)
+        t.start()
+
+        #self.socket.close()
+        #self.robot.close()
 
 
 if __name__ == "__main__":
-    s = server()
+    s = Server()
     s.init_server()
 
