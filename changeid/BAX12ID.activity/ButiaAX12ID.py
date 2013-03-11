2c28d50edd1a2a3a517f05f86a8beb0c9a265518
Rodrigo Dearmas <dearmas@fing.edu.uy>
2013-03-11 19:27:44 -0300

    stable version - Messages added

--- changeid/BAX12ID.activity/ButiaAX12ID.py
+++ changeid/BAX12ID.activity/ButiaAX12ID.py
@@ -67,8 +67,6 @@ class ButiaAX12ID(activity.Activity):
         fim = gtk.Fixed()
         fcom = gtk.Fixed()
         
-        #combo = gtk.ComboBox()
-
         img.set_from_file("activity/wall.svg")
         img.show()
 
@@ -80,69 +78,59 @@ class ButiaAX12ID(activity.Activity):
         
 		#Boton change ID left motor
         button_acceptl = gtk.Button(_("Change ID (LEFT motor)"))
-        #button_acceptl.set_size_request(100, 100)
         button_acceptl.connect("clicked", self.warning_messageIDL)
         #Boton change ID right motor
         button_acceptr = gtk.Button(_("Change ID (RIGHT motor)"))
-        #button_acceptr.set_size_request(100, 100)
         button_acceptr.connect("clicked", self.warning_messageIDR)
         #Boton change ID motor
-        button_accept = gtk.Button(_("Change ID"))
-        #button_acceptr.set_size_request(100, 100)
+        button_accept = gtk.Button(_("Change ID (CUSTOM)"))
         button_accept.connect("clicked", self.warning_messageID)
-
-        
-        #bcb = ComboID()
-
-
-        # ID combobox
+        # Combo change custom ID
         l = gtk.ListStore(str)
         combo = gtk.ComboBox(l)
         cell = gtk.CellRendererText()
         combo.pack_start(cell, True)
         combo.add_attribute(cell, 'text', 0)
         combo.append_text('Select ID:')
+        #list of available IDs
         for i in range(0, 253):
             combo.append_text(str(i))
         combo.set_active(0)
-
         def changed_cb(self):
-            #self.sel = combo.get_active()
             model = combo.get_model()
             index = combo.get_active()
             if index:
                 ButiaAX12ID.sel = model[index][0]
             return
-
         combo.connect('changed', changed_cb)
         
-
-
-
-
         fbcidl.add(button_acceptl)
         fbcidr.add(button_acceptr)
         fbcid.add(button_accept)
         fcom.add(combo)
         
-        fbcidl.put(button_acceptl, 10, 20)
-        fbcidr.put(button_acceptr, 10, 20)
-        fbcid.put(button_accept, 10, 20)
-        fcom.put(combo, 10, 20)
+
+
+###############################################################################
+######################HACER LOS LAYOUTS SIZEABLES##############################
+###############################################################################
+        #Button alignment 
+        fbcidl.put(button_acceptl, 10, 5)
+        fbcidr.put(button_acceptr, 10, -100)
+        fcom.put(combo, 10, 5)
+        fbcid.put(button_accept, 10, -100)
         fim.put(img, 1, 20)
 
         vbox.add(fbcidl)
         vbox.add(fbcidr)
-        vbox.add(fbcid)
         vbox.add(fcom)
+        vbox.add(fbcid)
         box.add(vbox)
         box.add(fim)
         box.show_all()
-
         self.set_canvas(box)
-#	return box
   
-	#levantar bobot
+	#bobot
     def bobot_launch(self):
 
         print 'Initialising butia...'
@@ -155,85 +143,106 @@ class ButiaAX12ID(activity.Activity):
                 self.bobot = subprocess.Popen(['./lua', 'bobot-server.lua'], cwd='./lib/butia_support')
             except:
                 print 'ERROR creating bobot'
-
         time.sleep(1)
-
         self.butia = butiaAPI.robot()
-
         self.modules = self.butia.get_modules_list()
-
         if (self.modules != []):
             print self.modules
         else:
             print _('Butia robot was not detected')
 
-
-
-		  #Mensaje change id left
+	#change left id message
     def warning_messageIDL(self, widget):
         msg = _('Please connect ONLY the LEFT motor to the board, and the power to this motor.\nNot disconnect the board and not close this activity.\nDo you want to continue?')
         dialog = gtk.MessageDialog(self, 0, gtk.MESSAGE_WARNING, gtk.BUTTONS_OK_CANCEL, msg)
         dialog.set_title(_('Changing motor ID...'))
         res = dialog.run()
         dialog.destroy()
-
-
         self.bobot_launch()
-
         if res == gtk.RESPONSE_OK:
-            #dialog = self.initing()
-            self.butia.write_info('254', '3', '1' )
-            if self.butia:
-                self.butia.close()
-                self.butia.closeService()
-            if self.bobot:
-                self.bobot.kill()
-				
+            self.butia.write_info('254', '3', '1' )           
+            time.sleep(1)
+            check = self.butia.write_info('1', '25', '1')
+            time.sleep(1)
+            self.butia.write_info('1', '25', '0')
+            #print check
+            if check == 1:
+                msg1 = _('ID Change CORRECT\nNew motor ID: 1')
+                dialog1 = gtk.MessageDialog(self, 0, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, msg1)
+                dialog1.set_title(_('Information'))
+                res1 = dialog1.run()
+                dialog1.destroy()
+
         elif res ==  gtk.RESPONSE_CANCEL:
             pass
+
+        
+        
+        if self.butia:
+            self.butia.close()
+            self.butia.closeService()
+        if self.bobot:
+            self.bobot.kill()
 		  
-		  #mensaje change id right
+    
+    #change right id message
     def warning_messageIDR(self, widget):
         msg = _('Please connect ONLY the RIGHT motor to the board, and the power to this motor.\nNot disconnect the board and not close this activity.\nDo you want to continue?')
         dialog = gtk.MessageDialog(self, 0, gtk.MESSAGE_WARNING, gtk.BUTTONS_OK_CANCEL, msg)
         dialog.set_title(_('Changing motor ID...'))
         res = dialog.run()
         dialog.destroy()
-
         self.bobot_launch()
-
         if res == gtk.RESPONSE_OK:
-            
-            #dialog = self.initing()
-				#prender led (broadcast, reg, value)
             self.butia.write_info('254', '3', '2' )
-            #self.butia.write_info('254', '25', '1' )
-            if self.butia:
-                self.butia.close()
-                self.butia.closeService()
-            if self.bobot:
-                self.bobot.kill()
-
+            time.sleep(1)
+            check = self.butia.write_info('2', '25', '1')
+            time.sleep(1)
+            self.butia.write_info('2', '25', '0')
+            #print check
+            if check == 1:
+                msg1 = _('ID Change CORRECT.\nNew motor ID: 2')
+                dialog1 = gtk.MessageDialog(self, 0, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, msg1)
+                dialog1.set_title(_('Information'))
+                res1 = dialog1.run()
+                dialog1.destroy()
         elif res ==  gtk.RESPONSE_CANCEL:
             pass
 
-		  #mensaje change id
+        if self.butia:
+            self.butia.close()
+            self.butia.closeService()
+        if self.bobot:
+            self.bobot.kill()
+
+
+    #change custom id message
     def warning_messageID(self, widget):
         msg = _('Please connect to the board ONLY the motor or motors that you want to change it id.\nYour motor''s new ID will be ' + str(ButiaAX12ID.sel) + '\nNot disconnect the board and not close this activity.\nDo you want to continue?')
         dialog = gtk.MessageDialog(self, 0, gtk.MESSAGE_WARNING, gtk.BUTTONS_OK_CANCEL, msg)
         dialog.set_title(_('Changing motor ID...'))
         res = dialog.run()
         dialog.destroy()
-
         self.bobot_launch()
-
         if res == gtk.RESPONSE_OK:
             self.butia.write_info('254', '3', str(ButiaAX12ID.sel) )
-            if self.butia:
-                self.butia.close()
-                self.butia.closeService()
-            if self.bobot:
-                self.bobot.kill()
-
+            time.sleep(1)
+            check = self.butia.write_info(str(ButiaAX12ID.sel), '25', '1')
+            time.sleep(1)
+            self.butia.write_info(str(ButiaAX12ID.sel), '25', '0')
+            #print check
+            if check == 1:
+                msg1 = _('ID Change CORRECT.\nNew motor ID: ' + str(ButiaAX12ID.sel))
+                dialog1 = gtk.MessageDialog(self, 0, gtk.MESSAGE_INFO, gtk.BUTTONS_OK, msg1)
+                dialog1.set_title(_('Information'))
+                res1 = dialog1.run()
+                dialog1.destroy()
         elif res ==  gtk.RESPONSE_CANCEL:
             pass
+        if self.butia:
+            self.butia.close()
+            self.butia.closeService()
+        if self.bobot:
+            self.bobot.kill()
+
+
