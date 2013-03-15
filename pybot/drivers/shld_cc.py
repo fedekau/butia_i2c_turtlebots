236b0c6c4a776ac384284bbc9c917628aee28c66
Alan Aguiar <alanjas@hotmail.com>
2013-03-15 18:16:47 -0300

    update shld_cc driver to new system

--- pybot/drivers/shld_cc.py
+++ pybot/drivers/shld_cc.py
@@ -8,8 +8,8 @@ def getVersion(dev):
     raw = dev.read(3)
     return raw[1] + raw[2] * 256
 
-def setvel2mtr(dev, sentido1, on1, sentido2, on2):
-    msg = [SET_VEL_2MTR, sentido1, on1, sentido2, on2]
+def setvel2mtr(dev, sentido1, vel1, sentido2, vel2):
+    msg = [SET_VEL_2MTR, sentido1, vel1 / 256, vel1 % 256, sentido2, vel2 / 256, vel2 % 256]
     dev.send(msg)
     raw = dev.read(1)
     return raw[0]
