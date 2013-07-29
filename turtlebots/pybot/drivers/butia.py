ed43c50086aca8a8862543a304b6157a3036ef91
Alan Aguiar <alanjas@hotmail.com>
2013-07-29 05:08:10 -0300

    fix lenght of getVersion of butia module

--- turtlebots/pybot/drivers/butia.py
+++ turtlebots/pybot/drivers/butia.py
@@ -4,8 +4,8 @@ GET_VOLT = 0x03
 
 def getVersion(dev):
     dev.send([RD_VERSION])
-    raw = dev.read(3)
-    return raw[1] + raw[2] * 256
+    raw = dev.read(2)
+    return raw[1]
 
 def getVolt(dev):
     dev.send([GET_VOLT])
