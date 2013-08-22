3b973c0c20d5c97d56ec5186fc4b4d4a901e4f9e
Alan Aguiar <alanjas@hotmail.com>
2013-08-22 18:50:38 -0300

    separate firmware for non sugar presence

--- firmware/ButiaFirmware.activity/ButiaFirmware.py
+++ firmware/ButiaFirmware.activity/ButiaFirmware.py
@@ -10,42 +10,8 @@ import ConfigParser
 import gettext
 from gettext import gettext as _
 
-try:
-    from sugar.activity import activity
-    from sugar.graphics.toolbarbox import ToolbarBox
-    from sugar.activity.widgets import ActivityToolbarButton
-    from sugar.activity.widgets import StopButton
-except Exception, err:
-    print 'No sugar', err
-
-import pybot
 from pybot import usb4butia
 
-
-class ButiaFirmware(activity.Activity):
-
-    def __init__(self, handle):
-        activity.Activity.__init__(self, handle)
-        self.build_toolbar()
-        flash = Flash(self)
-        self.set_canvas(flash.build_canvas())
-        self.show_all()
-
-    def build_toolbar(self):
-        toolbox = ToolbarBox()
-        activity_button = ActivityToolbarButton(self)
-        toolbox.toolbar.insert(activity_button, -1)
-        activity_button.show()
-        separator = gtk.SeparatorToolItem()
-        separator.props.draw = False
-        separator.set_expand(True)
-        toolbox.toolbar.insert(separator, -1)
-        separator.show()
-        stop_button = StopButton(self)
-        toolbox.toolbar.insert(stop_button, -1)
-        stop_button.show()
-        self.set_toolbox(toolbox)
-        toolbox.show()
 
 class Flash():
 
