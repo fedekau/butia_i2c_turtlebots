47ca36f04b3769975e017f0fcf9db5c64f247b8b
Alan Aguiar <alanjas@hotmail.com>
2013-08-28 20:08:20 -0300

    some fixes in ax driver

--- turtlebots/pybot/drivers/ax.py
+++ turtlebots/pybot/drivers/ax.py
@@ -18,7 +18,7 @@ def writeInfo(dev, motor_id, regstart, value):
 def readInfo(dev, motor_id, regstart, lenght):
     msg = [READ_INFO, motor_id, regstart, lenght]
     dev.send(msg)
-    raw = dev.read(lenght + 1)
+    raw = dev.read(3)
     if lenght == 1:
         return raw[1]
     else:
@@ -46,35 +46,38 @@ def sendPacket(dev, pack):
 def wheelMode(dev, motor_id):
     msg = [WRITE_INFO, motor_id, 0x06, 0x00, 0x00]
     dev.send(msg)
-    raw = dev.read(1)
+    raw = dev.read(2)
     msg = [WRITE_INFO, motor_id, 0x08, 0x00, 0x00]
     dev.send(msg)
-    raw = dev.read(1)
+    raw = dev.read(2)
     return raw[1]
 
 def jointMode(dev, motor_id, _min, _max):
     msg = [WRITE_INFO, motor_id, 0x06, _min / 256, _min % 256]
     dev.send(msg)
-    raw = dev.read(1)
+    raw = dev.read(2)
     msg = [WRITE_INFO, motor_id, 0x08, _max / 256, _max % 256]
     dev.send(msg)
-    raw = dev.read(1)
+    raw = dev.read(2)
+    return raw[1]
 
 def setPosition(dev, motor_id, pos):
     msg = [WRITE_INFO, motor_id, 0x1E, pos / 256, pos % 256]
     dev.send(msg)
-    raw = dev.read(1)
+    raw = dev.read(2)
+    return raw[1]
 
 def getPosition(dev, motor_id):
     msg = [READ_INFO, motor_id, 0x24, 2]
     dev.send(msg)
     raw = dev.read(3)
-    raw_angle = raw[2] * 256 + raw[3]
+    raw_angle = raw[1] * 256 + raw[2]
     return raw_angle * 0.29
 
 def setSpeed(dev, motor_id, speed):
     vel = speed * 1.496
-    msg = [motor_id, 0x20, speed / 256, speed % 256]
+    msg = [WRITE_INFO, motor_id, 0x20, speed / 256, speed % 256]
     dev.send(msg)
-    raw = dev.read(1)
+    raw = dev.read(2)
+    return raw[1]
 
